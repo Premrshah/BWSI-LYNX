ddcd8d881ba23ba79203d9e818cda890cdbd1693
Lynx <jacoby.adwin@gmail.com>
2025-07-24 16:44:16 -0400

    Initialize from Pi5

---
+++ src/camera_launcher/launch/launch_camera.launch.py
@@ -0,0 +1,21 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    front_camera = Node(
+        package='camera_ros',
+        executable='camera_node',
+        name='front_cam',
+        parameters=[{'camera':0}],
+        output='screen'
+    )
+
+    down_camera = Node(
+        package='camera_ros',
+        executable='camera_node',
+        name='down_cam',
+        parameters=[{'camera':1}],
+        output='screen'
+    )
+
+    return LaunchDescription([front_camera, down_camera])
