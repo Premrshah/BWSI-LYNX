956911f787411c58e598327180d302412c92bcf8
jacobyia <jacoby.adwin@gmail.com>
2025-07-28 16:16:39 -0400

    Flip image in dc??

--- src/vision_processors/vision_processors/line_detector.py
+++ src/vision_processors/vision_processors/line_detector.py
@@ -54,6 +54,8 @@ class LineDetector(Node):
         try:
             # Convert Image msg to OpenCV image
             image = self.bridge.imgmsg_to_cv2(msg, "mono8")
+            
+            image = cv2.flip(image, -1)
 
             # Detect line in the image. detect returns a parameterize the line (if one exists)
             line = self.detect_line(image)
